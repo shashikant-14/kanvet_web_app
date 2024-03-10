260a535a59b9b9420d65fd8fcd77b874f47ebee0
shashikant <pshashikant1998@gmail/com>
2024-03-10 23:02:29 +0530

    implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React, { Fragment } from 'react'
-import Home from './components/home/home'
-import Header from './components/header/Header'
-import About from './components/about/About'
-import Contact from './components/contact/Contact'
-import Products from './components/products/Products'
+import React, { Fragment, Suspense, lazy } from 'react'
+// import Home from './components/home/home'
+// import Header from './components/header/Header'
+// import About from './components/about/About'
+// import Contact from './components/contact/Contact'
+// import Products from './components/products/Products'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+
+const Home = lazy(()=>import('./components/home/Home'));
+const About = lazy(()=>import('./components/about/About'));
+const Contact = lazy(()=>import('./components/contact/Contact'));
+const Products = lazy(()=>import('./components/products/Products'));
+const Header = lazy(()=>import('./components/header/Header'));
+
+
 
 const App = () => {
   return (
     <Fragment>
-
+      <Suspense fallback={<div>Loading...</div>}>
       <Router>
         <Header />
         <Routes>
@@ -20,7 +28,7 @@ const App = () => {
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
-
+      </Suspense>
     </Fragment>
   )
 }
