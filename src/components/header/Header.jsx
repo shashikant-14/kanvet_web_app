1531f157c8be48778b39aab2b98a70d7d1bfa17b
shashikant <pshashikant1998@gmail/com>
2024-03-10 13:20:11 +0530

    home page updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
+import '../../App.css'
 
 const Header = () => {
   const location = useLocation();
@@ -13,14 +14,13 @@ const Header = () => {
   };
 
   const activeStyle = {
-    fontWeight: 400,
+    fontWeight: 600,
     fontSize: 'larger',
     color: 'rgb(20, 130, 160)',
     marginRight: '50px',
-    // textDecoration: 'underline rgb(217, 190, 136) 500'
-    paddingBottom:'0px',
-    borderBottom:'5px rgb(217, 190, 136) solid'
-  }
+    paddingBottom: '0px',
+    borderBottom: '5px solid rgb(217, 190, 136)',
+  };
 
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-white py-3'>
@@ -43,16 +43,16 @@ const Header = () => {
 
         <div className='collapse navbar-collapse flex-row-reverse' id='navbarNav'>
           <div className='navbar-nav ml-auto'>
-            <NavLink to='/home' className='nav-link' style={location.pathname == '/home' ? activeStyle : menuTextStyle}>
+            <NavLink to='/home' className='nav-link' style={location.pathname === '/home' ? activeStyle : menuTextStyle}>
               Home
             </NavLink>
-            <NavLink to='/about' className='nav-link' style={location.pathname == '/about' ? activeStyle : menuTextStyle}>
+            <NavLink to='/about' className='nav-link' style={location.pathname === '/about' ? activeStyle : menuTextStyle}>
               About
             </NavLink>
-            <NavLink to='/products' className='nav-link' style={location.pathname == '/products' ? activeStyle : menuTextStyle}>
+            <NavLink to='/products' className='nav-link' style={location.pathname === '/products' ? activeStyle : menuTextStyle}>
               Products
             </NavLink>
-            <NavLink to='/contact' className='nav-link' style={location.pathname == '/contact' ? activeStyle : menuTextStyle}>
+            <NavLink to='/contact' className='nav-link' style={location.pathname === '/contact' ? activeStyle : menuTextStyle}>
               Contact
             </NavLink>
           </div>
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Front from '../../assets/Front.png';
 
 const Home = () => {
   return (
-    <div className='text-center'>
-      home page is under development
+    <div className='row text-center p-0 m-0'>
+      <div className='col-12 p-0 m-0'>
+        <img src={Front} className='img-fluid border-0 p-0 m-0' style={{ width: '100%', height: '550px' }} alt='Kanvet Front Logo' title='Kanvet Front Logo' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
